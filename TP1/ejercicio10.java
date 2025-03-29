234eb90c06958fd5253f54e654b9a738fa90f6b6
Mailen_Ramos <mailenramos7gmail.com>
2025-03-29 00:19:05 -0300

    subiendo todos los ejercicios excepto el 12 del tp1

--- TP1/ejercicio10.java
+++ TP1/ejercicio10.java
@@ -4,5 +4,14 @@ a. si el primero es mayor al segundo.
 b. si ambos son múltiplos de 2.
  */
 public class ejercicio10 {
+    public static void main(String[] args) {
+        double num1=0,num2=0;
+        System.out.println("ingrese 2 numeros reales");
+        num1=Utils.leerDouble();
+        num2=Utils.leerDouble();
+        System.out.println("El primer numero es mayor al segundo? "+(num1>num2));
+        System.out.println("Ambos numeros son multiplos de 2? "+((num1%2==0)&&(num2%2==0)));
+    }
     
 }
+        
