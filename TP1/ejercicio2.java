4f93029440208f37b1b239e997ebf95166795f86
Mailen_Ramos <mailenramos7gmail.com>
2025-03-28 23:35:06 -0300

    ejercicio7 tp1

--- TP1/ejercicio2.java
+++ TP1/ejercicio2.java
@@ -1,7 +1,7 @@
-   /*Escribir un programa que declare una variable de tipo double y una
-constante de tipo double con valor 1.0. Luego, deberá asignar el
-doble del valor de la constante a la variable y mostrar ambos por
-pantalla.
+   /*Escribir un programa que declare una variable de tipo double 
+   y una constante de tipo double con valor 1.0.
+    Luego, deberá asignar el doble del valor de la constante a la variable 
+    y mostrar ambos por pantalla.
  */
 public class ejercicio2 {
  
